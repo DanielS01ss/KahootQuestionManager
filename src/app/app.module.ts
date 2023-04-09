803c6c574a783afe05212b4ca1fe65b712e3d9d1
Daniel Stanculescu <danystanculescu@gmail.com>
2023-04-09 17:52:18 +0300

    Added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { AllTasksComponent } from './all-tasks/all-tasks.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { LoginBoardComponent } from './login-board/login-board.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    AllTasksComponent,
+    AddTaskComponent,
+    LoginBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login-board/login-board.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-board',
+  templateUrl: './login-board.component.html',
+  styleUrls: ['./login-board.component.css']
+})
+export class LoginBoardComponent {
+  username=""
+  show=true
+  password=""
+  submit(){
+
+  }
+  
+}
