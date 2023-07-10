d972f3c1b87f3e1e6f665c1eff2cadcd8e0c807d
Daniel Stanculescu <danystanculescu@gmail.com>
2023-07-10 20:51:14 +0300

    Code update

--- src/app/constants/apiEndpoints.ts
+++ src/app/constants/apiEndpoints.ts
@@ -1,5 +1,5 @@
 
-const baseUrl = 'https://kahootbe2.onrender.com';
+const baseUrl = 'http://localhost:5000';
 export const API_ENDPOINTS = {
     login:`${baseUrl}/auth/login`,
     getAllQuestions:`${baseUrl}/questions/all`,
